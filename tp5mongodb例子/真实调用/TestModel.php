264291380926e67161fb277a26528f470361a522
mojianhua <1657210793@qq.com>
2018-08-08 16:02:40 +0800

    TP，新增。更新，删除，查询调用

--- tp5mongodb例子/真实调用/TestModel.php
+++ tp5mongodb例子/真实调用/TestModel.php
@@ -7,18 +7,35 @@ namespace app\domain\model;
 
 class TestModel extends MongoDbBaseModel
 {
-    protected $name = 'sell';
-    protected $pk = 'id';
+    protected $name = 'test1';
 
-    public function testlist(){
-        return self::select();
+    public function sell_list(){
+        $all = $this->select();
+        //$where['sid|name'] = 5;
+        //$where['sid'] = ['between',['1','10']];
+        //$all = $this->where($where)->field('name,sid')->select();
+        return toarr($all);
     }
 
     public function sell_add(){
-        $data['id'] = 2;
-        $data['name'] = 'test123';
+        $data['sid'] = 5;
+        $data['name'] = 'sell555';
+        $data['age'] = '555';
         $save = $this->save($data);
         var_dump($save);
+    }
 
+    public function sell_update(){
+        $sell = new TestModel();
+        $sell = $sell->where('sid',3)->find();
+        $data['name'] = 'sell3333';
+        $save = $sell->save($data);
+        var_dump($save);
+    }
+
+    public function sell_del(){
+        $sell = new TestModel();
+        $del = $sell->where('sid',3)->delete();
+        var_dump($del);
     }
 }
